7b7b9f831e0c76a011376566bf457ffbf560c214
Barton Willis <willisb@unk.edu>
2026-09-24 08:43:44 -0500

    Update ortho_display.lisp
    - change $hermite to %hermite, and ...  in the display code

--- ortho_display.lisp
+++ ortho_display.lisp
@@ -60,67 +60,67 @@
   (setq form `(( ((mprogn) ,(nth 1 form)) simp array) ,(nth 2 form)))
   (dimension-array form result))
 
-(defprop $jacobi_p tex-jacobi-poly tex)
+(defprop %jacobi_p tex-jacobi-poly tex)
 
 (defun tex-jacobi-poly (x l r)
   (tex-sub-and-super-scripted-function "P" `(0) nil `(1 2) t 3 x l r))
 
-(setf (get '$jacobi_p 'dimension) 'dimension-jacobi-p)
+(setf (get '%jacobi_p 'dimension) 'dimension-jacobi-p)
 
 (defun dimension-jacobi-p (form result)
   (dimension-function
    (dimension-sub-and-super-scripted-function '|$p| `(1) `(2 3) t 4 form)
    result))
    
-(defprop $ultraspherical tex-ultraspherical tex)
+(defprop %ultraspherical tex-ultraspherical tex)
 
 (defun tex-ultraspherical (x l r)
   (tex-sub-and-super-scripted-function "C" `(0) nil `(1) t 2 x l r))
 
-(setf (get '$ultraspherical 'dimension) 'dimension-ultraspherical)
+(setf (get '%ultraspherical 'dimension) 'dimension-ultraspherical)
 
 (defun dimension-ultraspherical (form result)
     (dimension-function
      (dimension-sub-and-super-scripted-function '|$c| `(1) `(2) t 3 form)
      result))
 
-(defprop $chebyshev_t tex-chebyshev-t tex)
+(defprop %chebyshev_t tex-chebyshev-t tex)
 
 (defun tex-chebyshev-t (x l r)
   (tex-sub-and-super-scripted-function "T" `(0) nil nil nil 1 x l r))
 
-(setf (get '$chebyshev_t 'dimension) 'dimension-chebyshev-t)
+(setf (get '%chebyshev_t 'dimension) 'dimension-chebyshev-t)
 
 (defun dimension-chebyshev-t (form result)
   (dimension-function
    (dimension-sub-and-super-scripted-function '|$t| `(1) nil nil 2 form)
    result))
 
-(defprop $chebyshev_u tex-chebyshev-u tex)
+(defprop %chebyshev_u tex-chebyshev-u tex)
 
 (defun tex-chebyshev-u (x l r)
   (tex-sub-and-super-scripted-function "U" `(0) nil nil nil 1 x l r))
 
-(setf (get '$chebyshev_u 'dimension) 'dimension-chebyshev-u)
+(setf (get '%chebyshev_u 'dimension) 'dimension-chebyshev-u)
 
 (defun dimension-chebyshev-u (form result)
   (dimension-function
    (dimension-sub-and-super-scripted-function '|$u| `(1) nil nil 2 form)
    result))
 
-(defprop $legendre_p tex-legendre-p tex)
+(defprop %legendre_p tex-legendre-p tex)
 
 (defun tex-legendre-p (x l r)
   (tex-sub-and-super-scripted-function "P" `(0) nil nil nil 1 x l r))
 
-(setf (get '$legendre_p 'dimension) 'dimension-legendre-p)
+(setf (get '%legendre_p 'dimension) 'dimension-legendre-p)
 
 (defun dimension-legendre-p (form result)
   (dimension-function
    (dimension-sub-and-super-scripted-function '|$p| `(1) nil nil 2 form)
    result))
 
-(defprop $legendre_q tex-legendre-q tex)
+(defprop %legendre_q tex-legendre-q tex)
 
 (defun tex-legendre-q (x l r)
   (tex-sub-and-super-scripted-function "Q" `(0) nil nil nil 1 x l r))
@@ -137,26 +137,26 @@
 (defun tex-assoc-legendre-q (x l r)
   (tex-sub-and-super-scripted-function "Q" `(0) nil `(1) nil 2 x l r))
 
-(setf (get '$assoc_legendre_q 'dimension) 'dimension-assoc-legendre-q)
+(setf (get '%assoc_legendre_q 'dimension) 'dimension-assoc-legendre-q)
 
 (defun dimension-assoc-legendre-q (form result)
  (dimension-function
    (dimension-sub-and-super-scripted-function '|$q| `(1) `(2) nil 3 form)
    result))
 
-(defprop $assoc_legendre_p tex-assoc-legendre-p tex)
+(defprop %assoc_legendre_p tex-assoc-legendre-p tex)
 
 (defun tex-assoc-legendre-p (x l r)
   (tex-sub-and-super-scripted-function "P" `(0) nil `(1) nil 2 x l r))
 
-(setf (get '$assoc_legendre_p 'dimension) 'dimension-assoc-legendre-p)
+(setf (get '%assoc_legendre_p 'dimension) 'dimension-assoc-legendre-p)
 
 (defun dimension-assoc-legendre-p (form result)
   (dimension-function
    (dimension-sub-and-super-scripted-function '|$p| `(1) `(2) nil 3 form)
    result))
 
-(defprop $gen_laguerre tex-gen-laguerre tex)
+(defprop %gen_laguerre tex-gen-laguerre tex)
 
 (defun tex-gen-laguerre (x l r)
   (tex-sub-and-super-scripted-function "L" `(0) nil `(1) t 2 x l r))
@@ -168,81 +168,81 @@
    (dimension-sub-and-super-scripted-function '|$l| `(1) `(2) t 3 form)
    result))
 
-(defprop $laguerre tex-laguerre tex)
+(defprop %laguerre tex-laguerre tex)
 
 (defun tex-laguerre (x l r)
   (tex-sub-and-super-scripted-function "L" `(0) nil nil nil 1 x l r))
 
-(setf (get '$laguerre 'dimension) 'dimension-laguerre)
+(setf (get '%laguerre 'dimension) 'dimension-laguerre)
 
 (defun dimension-laguerre (form result)
   (dimension-function
    (dimension-sub-and-super-scripted-function '|$l| `(1) nil nil 2 form)
    result))
 
-(defprop $spherical_hankel1 tex-spherical-hankel-1 tex)
+(defprop %spherical_hankel1 tex-spherical-hankel-1 tex)
 
 (defun tex-spherical-hankel-1 (x l r)
   (tex-sub-and-super-scripted-function "h^{(1)}" `(0) nil nil nil 1 x l r))
 
-(setf (get '$spherical_hankel1 'dimension) 'dimension-spherical-hankel-1)
+(setf (get '%spherical_hankel1 'dimension) 'dimension-spherical-hankel-1)
 
 (defun dimension-spherical-hankel-1 (form result)
   (let ((form1 `((mexpt) (($\h simp array) ,(nth 1 form)) 
 		 (1))))
     (dimension-function `((,form1 simp) ,(nth 2 form)) result)))
 
-(defprop $spherical_hankel2 tex-spherical-hankel-2 tex)
+(defprop %spherical_hankel2 tex-spherical-hankel-2 tex)
 
 (defun tex-spherical-hankel-2 (x l r)
   (tex-sub-and-super-scripted-function "h^{(2)}" `(0) nil nil nil 1 x l r))
 
-(setf (get '$spherical_hankel2 'dimension) 'dimension-spherical-hankel-2)
+(setf (get '%spherical_hankel2 'dimension) 'dimension-spherical-hankel-2)
 
 (defun dimension-spherical-hankel-2 (form result)
   (let ((form1 `((mexpt) (($\h simp array) ,(nth 1 form))  (2))))
     (dimension-function `((,form1 simp) ,(nth 2 form)) result)))
   
-(defprop $spherical_bessel_j tex-spherical-bessel-j tex)
+(defprop %spherical_bessel_j tex-spherical-bessel-j tex)
 
 (defun tex-spherical-bessel-j (x l r)
   (tex-sub-and-super-scripted-function "j^{(2)}" `(0) nil nil nil 1 x l r))
 
-(setf (get '$spherical_bessel_j 'dimension) 'dimension-spherical-bessel-j)
+(setf (get '%spherical_bessel_j 'dimension) 'dimension-spherical-bessel-j)
 
 (defun dimension-spherical-bessel-j (form result)
   (let ((form1 `(($\j simp array) ,(nth 1 form)))) 
     (dimension-function `((,form1 simp) ,(nth 2 form)) result)))
 
-(defprop $spherical_bessel_y tex-spherical-bessel-y tex)
+(defprop %spherical_bessel_y tex-spherical-bessel-y tex)
 
 (defun tex-spherical-bessel-y (x l r)
   (tex-sub-and-super-scripted-function "y^{(2)}" `(0) nil nil nil 1 x l r))
 
- (setf (get '$spherical_bessel_y 'dimension) 'dimension-spherical-bessel-y)
+ (setf (get '%spherical_bessel_y 'dimension) 'dimension-spherical-bessel-y)
 
 (defun dimension-spherical-bessel-y (form result)
   (let ((form1 `(($\y simp array) ,(nth 1 form)))) 
     (dimension-function `((,form1 simp) ,(nth 2 form)) result)))
 
-(defprop $spherical_harmonic tex-spherical-harmonic tex)
+(defprop %spherical_harmonic tex-spherical-harmonic tex)
 
 (defun tex-spherical-harmonic (x l r)
   (tex-sub-and-super-scripted-function "Y" `(0) nil `(1) nil 2 x l r))
 
-(setf (get '$spherical_harmonic 'dimension) 'dimension-spherical-harmonic)
+(setf (get '%spherical_harmonic 'dimension) 'dimension-spherical-harmonic)
 
 (defun dimension-spherical-harmonic (form result)
  (dimension-function
   (dimension-sub-and-super-scripted-function '|$y| `(1) `(2) nil 3 form)
   result))
   
-(defprop $hermite tex-hermite tex)
+(defprop %hermite tex-hermite tex)
 
 (defun tex-hermite (x l r)
   (tex-sub-and-super-scripted-function "H" `(0) nil nil nil 1 x l r))
 
-(setf (get '$hermite 'dimension) 'dimension-hermite)
+(setf (get '%hermite 'dimension) 'dimension-hermite)
 
 (defun dimension-hermite (form result)
  (dimension-function
